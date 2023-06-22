7816431b7e240b32eec81e2d0bbb575c87ac1bae
Dimitar Todorov <d.todorov.vn@gmail.com>
2023-06-22 15:22:48 +0300

    Jetpack compose article exercise

--- ComposeBasicsExercises/app/src/main/java/com/example/composebasicsexercises/MainActivity.kt
+++ ComposeBasicsExercises/app/src/main/java/com/example/composebasicsexercises/MainActivity.kt
@@ -3,13 +3,22 @@ package com.example.composebasicsexercises
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composebasicsexercises.ui.theme.ComposeBasicsExercisesTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +31,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Article(
+                        title = stringResource(R.string.title),
+                        textOne = stringResource(R.string.text_one),
+                        textTwo = stringResource(R.string.text_two)
+                    )
                 }
             }
         }
@@ -30,17 +43,45 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Article(title: String,
+            textOne: String,
+            textTwo: String,
+            modifier: Modifier = Modifier
+) {
+    Column{
+        val image = painterResource(R.drawable.bg_compose_background)
+        Image(painter = image, contentDescription = null)
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+
+        Text(
+            text = textOne,
+            modifier
+                .padding(start = 16.dp, end = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+
+        Text(
+            text = textTwo,
+            modifier
+                .padding(16.dp),
+            textAlign = TextAlign.Justify
+
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComposeBasicsExercisesTheme {
-        Greeting("Android")
+        Article(
+            title = stringResource(R.string.title),
+            textOne = stringResource(R.string.text_one),
+            textTwo = stringResource(R.string.text_two)
+        )
     }
 }
